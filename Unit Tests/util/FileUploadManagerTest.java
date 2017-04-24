2b484e13d8eb7148a2e2b81f368ebd12a88ae0df
Kelly <kmuhindi@jkuat.ac.ke>
2017-04-24 13:12:49 +0300

    Refactor file upload functionality (more reusable)

--- Unit Tests/util/FileUploadManagerTest.java
+++ Unit Tests/util/FileUploadManagerTest.java
@@ -6,16 +6,31 @@
  */
 package util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import org.junit.After;
+import org.junit.Assert;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  *
  * @author kelly
  */
+@RunWith(MockitoJUnitRunner.class)
 public class FileUploadManagerTest {
+    
+//    @InjectMocks
+    
+    @Mock
+    private FileUploadManager fm;
+    @Mock
+    private InputStream inputStream;
     
     public FileUploadManagerTest() {
     }
@@ -27,4 +42,28 @@ public class FileUploadManagerTest {
     @After
     public void tearDown() {
     }
+    
+    @Test
+    public void testFileUploadObjNutNull(){
+        Assert.assertNotNull("Object is null",fm);
+    }
+    
+    @Test
+    public void testUploadSuccess() throws IOException{
+        when(fm.upload("file2", inputStream)).thenReturn(true);
+        assertTrue(fm.upload("file2", inputStream));
+    }
+    
+    @Test
+    public void testUploadFail() throws IOException{
+        when(fm.upload("file", inputStream)).thenReturn(Boolean.FALSE);
+        Assert.assertFalse(fm.upload("file", inputStream));
+    }
+    
+    @Test(expected = IOException.class)
+    public void testUploadThrowsException() throws IOException{
+        when(fm.upload("file2", inputStream)).thenThrow(new IOException("Error"));
+        fm.upload("file2", inputStream);
+    }
+
 }
